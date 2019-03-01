d0f2a682b4ff9398112ede4dd9d445fe769c231a
nnnlog <sorisem4106@naver.com>
2019-03-01 10:47:51 +0900

    update

--- src/nlog/NLOGItemFrame/Main.php
+++ src/nlog/NLOGItemFrame/Main.php
@@ -2,69 +2,62 @@
 
 namespace nlog\NLOGItemFrame;
 
-use pocketmine\plugin\PluginBase;
-use pocketmine\event\Listener;
-use pocketmine\event\block\ItemFrameDropItemEvent;
 use ifteam\SimpleArea\database\area\AreaProvider;
 use ifteam\SimpleArea\database\area\AreaSection;
+use pocketmine\event\Listener;
+use pocketmine\event\server\DataPacketReceiveEvent;
+use pocketmine\network\mcpe\protocol\ItemFrameDropItemPacket;
+use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase implements Listener{
-	
-	const TAG = "§b§o[ Info ] §7";
-	public $area;
-	
- 	 public function onEnable(){
-    	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-    	
-    	if ($this->getServer()->getPluginManager()->getPlugin("SimpleArea") === null) {
-    		$this->getLogger()->notice("SimpleArea가 없습니다. SimpleArea 호환 모드를 종료합니다.");
-    		$this->getLogger()->notice("SimpleArea doesn't exist. SimpleArea compatibility mode turn off.");
-    		$this->area = false;
-    	}else{
-    		$this->area = true;
-    	}
-    	
-    	if ($this->getServer()->getLanguage()->getLang() === "kor") {
-    		$this->getLogger()->info("액자 아이템 드랍 금지 플러그인");
-    	}else{
-    		$this->getLogger()->info("Block Item Frame Plugin");
-    	}
-    	$this->getLogger()->info("Made by NLOG (nlog.kro.kr)");
- 	 }
- 	 
- 	 public function onDropItem (ItemFrameDropItemEvent $ev) {
- 	 	
- 	 	if (!$ev->getPlayer()->isOp()) {
- 	 		if ($this->area) {
- 	 			$areaSection = AreaProvider::getInstance()->getArea($ev->getBlock()->getLevel(), $ev->getBlock()->x, $ev->getBlock()->z);
- 	 			if ($areaSection instanceof AreaSection) {
- 	 				if(!$areaSection->isResident($ev->getPlayer()->getName())) {
- 	 					$ev->setCancelled(true);
- 	 					if ($this->getServer()->getLanguage()->getLang() === "kor") {
- 	 						$ev->getPlayer()->sendMessage(self::TAG."액자 내의 아이템을 뺄 수 없습니다.");
- 	 					}else{
- 	 						$ev->getPlayer()->sendMessage(self::TAG."You can't get item from item frame");
- 	 					}
- 	 				}
- 	 			}else{
- 	 				$ev->setCancelled(true);
- 	 				if ($this->getServer()->getLanguage()->getLang() === "kor") {
- 	 					$ev->getPlayer()->sendMessage(self::TAG."액자 내의 아이템을 뺄 수 없습니다.");
- 	 				}else{
- 	 					$ev->getPlayer()->sendMessage(self::TAG."You can't get item from item frame");
- 	 				}
- 	 			}
- 	 		}else{
- 	 			$ev->setCancelled(true);
- 	 			if ($this->getServer()->getLanguage()->getLang() === "kor") {
- 	 				$ev->getPlayer()->sendMessage(self::TAG."액자 내의 아이템을 뺄 수 없습니다.");
- 	 			}else{
- 	 				$ev->getPlayer()->sendMessage(self::TAG."You can't get item from item frame");
- 	 			}
- 	 		}
- 	 	}
- 	 	
- 	 }
- 	 
-  }
+class Main extends PluginBase implements Listener {
+
+    const TAG = "§b§l[ §f서버§b ] §r§7";
+    public $area;
+
+    public function onEnable() {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        if ($this->getServer()->getPluginManager()->getPlugin("SimpleArea") === null) {
+            $this->getLogger()->notice("SimpleArea가 없습니다. SimpleArea 호환 모드를 종료합니다.");
+            $this->getLogger()->notice("SimpleArea doesn't exist. SimpleArea compatibility mode turn off.");
+            $this->area = false;
+        } else {
+            $this->area = true;
+        }
+
+        if ($this->getServer()->getLanguage()->getLang() === "kor") {
+            $this->getLogger()->info("액자 아이템 드랍 금지 플러그인");
+        } else {
+            $this->getLogger()->info("Block Item Frame Plugin");
+        }
+        $this->getLogger()->info("Made by NLOG (nlog.kro.kr)");
+    }
+
+    public function onDropItem(DataPacketReceiveEvent $ev) {
+        /** @var ItemFrameDropItemPacket $pk */
+        if (($pk = $ev->getPacket()) instanceof ItemFrameDropItemPacket) {
+            if (!$ev->getPlayer()->isOp()) {
+                if ($this->area) {
+                    $areaSection = AreaProvider::getInstance()->getArea($ev->getPlayer()->getLevel(), $pk->x, $pk->z);
+                    if ($areaSection instanceof AreaSection) {
+                        if (!$areaSection->isResident($ev->getPlayer()->getName())) {
+                            $ev->setCancelled(true);
+                            $ev->getPlayer()->sendMessage(self::TAG . "액자 내의 아이템을 뺄 수 없습니다.");
+
+                        }
+                    } else {
+                        $ev->setCancelled(true);
+                        $ev->getPlayer()->sendMessage(self::TAG . "액자 내의 아이템을 뺄 수 없습니다.");
+                    }
+                } else {
+                    $ev->setCancelled(true);
+                    $ev->getPlayer()->sendMessage(self::TAG . "액자 내의 아이템을 뺄 수 없습니다.");
+                }
+            }
+
+        }
+
+    }
+}
+
 ?>
